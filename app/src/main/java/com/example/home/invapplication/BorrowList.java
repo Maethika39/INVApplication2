7e064fed132969f953f355d69c135309e431cf88
Win_10 <bluetaiyaki9339@gmail.com>
2017-05-09 18:57:06 +0700

    fix ListBorrow

--- app/src/main/java/com/example/home/invapplication/BorrowList.java
+++ app/src/main/java/com/example/home/invapplication/BorrowList.java
@@ -8,6 +8,6 @@ public class BorrowList extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_borrow_list);
+        setContentView(R.layout.activity_borrow_list2);
     }
 }
